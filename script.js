1a71cf68bc298bff991d2024110d32374bbb4cc6
Mohit Verma <123365283+mohitvermag@users.noreply.github.com>
2024-08-25 15:40:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ async function checkWeather(city){
     const response = await fetch(apiurl + city + `&appid=${apikey}`);
     if(response.status == 404){
         document.querySelector(".error").style.display = "block";
-        document.querySelector(".weather").style.display = "none";
     }
     else{
 
@@ -39,7 +38,6 @@ else if(data.weather[0].main == "Mist"){
 else if(data.weather[0].main == "Snow"){
     weathericon.src = "Images/snow.png";
 }
-document.querySelector(".weather").style.display = "block";
 document.querySelector(".error").style.display = "none";
 
     }
